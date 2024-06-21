ce4596df6b6d1df4e12f124cfe0d29d47fe818ce
Thishyaketh <thishyakethabimalla@gmail.com>
2024-06-21 17:42:41 +0530

    Update models.py

--- models.py
+++ models.py
@@ -8,11 +8,12 @@ class User(db.Model):
     username = db.Column(db.String(150), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(60), nullable=False)
-    profiles = db.relationship('Profile', backref='owner', lazy=True)
+    social_credentials = db.relationship('SocialCredential', backref='owner', lazy=True)
 
-class Profile(db.Model):
+class SocialCredential(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    social_media_name = db.Column(db.String(100), nullable=False)
-    social_media_url = db.Column(db.String(200), nullable=False)
+    platform = db.Column(db.String(100), nullable=False)
+    username = db.Column(db.String(100), nullable=False)
+    password = db.Column(db.String(100), nullable=False)
     created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
