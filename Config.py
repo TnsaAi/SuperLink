8f18a57a5f3df57810ee960c39175db8a747aa46
Thishyaketh <thishyakethabimalla@gmail.com>
2024-06-22 19:35:05 +0530

    Update Config.py

--- Config.py
+++ Config.py
@@ -4,3 +4,5 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'your_secret_key'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///site.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    CELERY_BROKER_URL = 'redis://localhost:6379/0'
+    CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
