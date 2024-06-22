7147339ac71c7b4924ea84612508ecdee792ea43
Thishyaketh <thishyakethabimalla@gmail.com>
2024-06-22 19:57:56 +0530

    Update Config.py

--- Config.py
+++ Config.py
@@ -4,3 +4,7 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'your_secret_key'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///site.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    OAUTHLIB_INSECURE_TRANSPORT = os.environ.get('OAUTHLIB_INSECURE_TRANSPORT') or '1'
+    GOOGLE_CLIENT_ID = os.environ.get('GOOGLE_CLIENT_ID') or 'your_google_client_id'
+    GOOGLE_CLIENT_SECRET = os.environ.get('GOOGLE_CLIENT_SECRET') or 'your_google_client_secret'
+    GOOGLE_DISCOVERY_URL = ("https://accounts.google.com/.well-known/openid-configuration")
