a7bfd70f21151cc807dd08b994a8d7d97e018680
Thishyaketh <thishyakethabimalla@gmail.com>
2024-06-26 18:01:42 +0530

    Update Config.py

--- Config.py
+++ Config.py
@@ -1,3 +1,4 @@
+#Configiration for SuperLink
 
 import os
 
@@ -5,7 +6,7 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'your_secret_key'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///site.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    OAUTHLIB_INSECURE_TRANSPORT = os.environ.get('OAUTHLIB_INSECURE_TRANSPORT') or '1'
     GOOGLE_CLIENT_ID = os.environ.get('GOOGLE_CLIENT_ID') or 'your_google_client_id'
     GOOGLE_CLIENT_SECRET = os.environ.get('GOOGLE_CLIENT_SECRET') or 'your_google_client_secret'
     GOOGLE_DISCOVERY_URL = ("https://accounts.google.com/.well-known/openid-configuration")
+    ZAPIER_WEBHOOK_URL = os.environ.get('ZAPIER_WEBHOOK_URL') or 'your_zapier_webhook_url'
